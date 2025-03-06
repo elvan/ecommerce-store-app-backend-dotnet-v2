42935191d677be9c3bb4e46a21203a59ea8df791
Elvan Hidayat <elvanhd@gmail.com>
2025-03-07 01:22:30 +0700

    Add order management with API endpoints and payment handling
    Integrated order functionality, including order creation, retrieval, and payment handling through Stripe. Added new controllers, DTOs, migrations, and services to support order processing and payment webhooks for success and failure scenarios.

--- API/Extensions/HttpExtensions.cs
+++ API/Extensions/HttpExtensions.cs
@@ -9,7 +9,7 @@ public static class HttpExtensions
 {
     public static void AddPaginationHeader(this HttpResponse response, PaginationMetadata metadata)
     {
-        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
         response.Headers.Append("Pagination", JsonSerializer.Serialize(metadata, options));
         response.Headers.Append(HeaderNames.AccessControlExposeHeaders, "Pagination");
